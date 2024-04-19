087f942764f0aa8169667999af526cb226465eb7
open dev <opendev.top@gmail.com>
2024-04-19 15:53:08 +0800

    Add getStaticPaths

--- next-crash/pages/article/[id]/index.js
+++ next-crash/pages/article/[id]/index.js
@@ -18,7 +18,19 @@ const article = ({ article }) => {
     )
 }
 
-export const getServerSideProps = async (context) => {
+// export const getServerSideProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+//     const article = await res.json()
+
+//     return {
+//         props: {
+//             article,
+//         },
+//     }
+// }
+
+export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
 
     const article = await res.json()
@@ -27,6 +39,21 @@ export const getServerSideProps = async (context) => {
         props: {
             article,
         },
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    const articles = await res.json()
+
+    const ids = articles.map((article) => article.id)
+    const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+    return {
+        // paths: { params: { id: "1", id: "2" } },
+        paths,
+        fallback: false,
     }
 }
 
