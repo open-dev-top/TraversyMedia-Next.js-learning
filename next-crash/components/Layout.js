b3f280281951905fb40a89a98563b7c352b9de56
open dev <opendev.top@gmail.com>
2024-04-19 11:18:01 +0800

    Add Navigation Bar

--- next-crash/components/Layout.js
+++ next-crash/components/Layout.js
@@ -1,11 +1,18 @@
 import React from "react"
 import styles from "../styles/Layout.module.css"
+import Nav from "./Nav"
 
 const Layout = ({ children }) => {
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>{children}</main>
-        </div>
+        <>
+            <Nav />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {/* <h1>Hello</h1> */}
+                    {children}
+                </main>
+            </div>
+        </>
     )
 }
 
